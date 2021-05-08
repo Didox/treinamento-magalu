9e2d8b3768ecc5919838d4b0ecf7ab43b85e4586
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-05-08 11:10:39 -0300

    subindo código php para produção

---
+++ index.php
@@ -0,0 +1,5 @@
+<h1>Olá meninas estamos fazendo um deploy de um sistema de forma clássica</h1>
+<h3>Treinamento Magalu - Gama Academy</h3>
+<div>
+  <b>Resultado da opração é:</b>  <? $i=324; $i = $i * 433; echo($i); ?>
+</div>
