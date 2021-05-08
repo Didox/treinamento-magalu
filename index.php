c38e0bee0ce08beca50691452f4d9a99f01271c5
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-05-08 11:53:19 -0300

    novo deploy

--- index.php
+++ index.php
@@ -3,7 +3,11 @@
 <div>
   <b>Resultado da opração é:</b>  <? $i=324; $i = $i * 433; echo($i); ?>
 </div>
-
+<br>
 <div style="background-color: blue;color:#fff;border-radius:5px;padding:30px">
   testando um ddeploy automático
+</div>
+<br>
+<div style="background-color: red;color:#fff;border-radius:5px;padding:30px">
+  testando um deploy automático
 </div>
